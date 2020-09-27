a141fcefdc1b82702364c116eb274d0c86cb0073
As-21 <as_21@live.com>
2020-09-27 11:13:06 +0300

    Added logic for working with the backend

--- src/App.js
+++ src/App.js
@@ -35,7 +35,26 @@ class App extends Component {
 			box: {},
 			route: 'signin',
 			isSignedIn: false,
+			user: {
+				id: '',
+				name: '',
+				email: '',
+				entries: 0,
+				joined: ''
+			}
 		}
+	}
+
+	loadUser = (data) => {
+		this.setState({
+			user: {
+				id: data.id,
+				name: data.name,
+				email: data.email,
+				entries: data.entries,
+				joined: data.joined
+			}
+		})
 	}
 
 	calculateFaceLocation = (data) => {
@@ -53,7 +72,6 @@ class App extends Component {
 	}
 
 	displayFaceBox = (box) => {
-		console.log(box);
 		this.setState({ box: box });
 	}
 
@@ -61,10 +79,25 @@ class App extends Component {
 		this.setState({ input: event.target.value });
 	}
 
-	onButtonSubmit = () => {
+	onPictureSubmit = () => {
 		this.setState({ imageUrl: this.state.input }, () => {
 			app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.imageUrl)
-				.then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+				.then(response => {
+					if (response) {
+						fetch('http://localhost:3000/image', {
+							method: 'put',
+							headers: { 'Content-Type': 'application/json' },
+							body: JSON.stringify({
+								id: this.state.user.id
+							})
+						})
+							.then(response => response.json())
+							.then(count => {
+								this.setState(Object.assign(this.state.user, { entries: count }))
+							})
+					}
+					this.displayFaceBox(this.calculateFaceLocation(response))
+				})
 				.catch(err => console.log(err));
 		});
 	}
@@ -87,14 +120,14 @@ class App extends Component {
 				{ route === 'home'
 					? <div>
 						<Logo />
-						<Rank />
-						<ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
+						<Rank name={this.state.user.name} entries={this.state.user.entries} />
+						<ImageLinkForm onInputChange={this.onInputChange} onPictureSubmit={this.onPictureSubmit} />
 						<FaceRecognition box={box} imageUrl={imageUrl} />
 					</div>
 					: (
 						route === 'signin' || route === 'signout'
-							? <SignIn onRouteChange={this.onRouteChange} />
-							: <Register onRouteChange={this.onRouteChange} />
+							? <SignIn onRouteChange={this.onRouteChange} loadUser={this.loadUser} />
+							: <Register onRouteChange={this.onRouteChange} loadUser={this.loadUser} />
 					)
 				}
 			</div>
